41f68c2890925611ef35e8b601b1c6a5d29dbc0a
CamCoop1 <cam.cooper.harris@gmail.com>
2025-05-05 20:27:13 +0930

    Completed days 1 and 2 of 2015

--- y2015/solutions/d01.rs
+++ y2015/solutions/d01.rs
@@ -1,35 +1,52 @@
-use utils::file_reader::read_lines;
-use utils::harness::Solve;
+use utils::{file_reader, harness::Solve};
 
 pub struct D01;
 
+ 
+fn count_characters(s : &str, character : char ) -> i16 {
+    s.chars().filter(|&c| c == character).count() as i16
+}
+ 
+
+fn determine_floor(input : &str) -> i16 { 
+    let go_up: i16 = count_characters(input, '(');
+    let go_down: i16 = count_characters(input, ')');
+
+    go_up - go_down
+    
+}
+
 impl Solve for D01 {
-    fn part1(_input: String, path: &String) -> String {
-        let mut floor = 0;
-        for char in read_lines(path).first().unwrap().chars() {
-            if char == ')' {
-                floor -= 1;
-            } else {
-                floor += 1;
-            }
+    fn part1(_: String, path: &String) -> String {
+        let input = file_reader::read_lines(path);
+        let mut counter:  i16 = 0;
+
+        for i in &input {
+            let floor = determine_floor(&i);
+            counter += floor
         }
-        return floor.to_string();
+
+        counter.to_string()
+        
     }
 
-    fn part2(_input: String, path: &String) -> String {
-        let mut floor = 0;
-        let mut basement_floor = 1;
-        for (i, char) in read_lines(path).first().unwrap().chars().enumerate() {
-            if char == ')' {
-                floor -= 1;
-            } else {
-                floor += 1;
+    fn part2(_: String, path: &String) -> String {
+        let input = file_reader::read_lines(path);
+        let mut counter:  i16 = 0;
+
+        for i in &input {
+            for (position, character) in i.chars().into_iter().enumerate() {
+                let mapped_position = position + 1;
+                counter += determine_floor(&character.to_string());
+                if counter == -1 {
+                    return mapped_position.to_string()
+                }
             }
-            if floor == -1 {
-                basement_floor += i;
-                break;
-            }
+         
+
         }
-        return basement_floor.to_string();
+
+        counter.to_string()
+        
     }
 }
--- y2015/solutions/d02.rs
+++ y2015/solutions/d02.rs
@@ -1,30 +1,109 @@
-use utils::file_reader::read_lines;
-use utils::harness::Solve;
+#![allow(dead_code, non_snake_case, unused)]
+
+use rayon::prelude::*;
+use utils::{file_reader, harness::Solve};
+
+type Result<T> = std::result::Result<T, Box<dyn std::error::Error>>;
 
 pub struct D02;
 
-impl Solve for D02 {
-    fn part1(_input: String, path: &String) -> String {
-        let mut sum = 0;
-        for line in read_lines(path) {
-            let v: Vec<i32> = line.split('x').map(|x| x.parse().unwrap()).collect();
-            let (x, y, z) = (v[0], v[1], v[2]);
-            let sides = vec![x * y, y * z, x * z];
-            sum += (2 * sides[0]) + (2 * sides[1]) + (2 * sides[2]);
-            sum += sides.iter().min().unwrap();
-        }
-        return sum.to_string();
+
+
+struct Rectangle {
+    length : u64,
+    height : u64, 
+    width : u64
+}
+
+impl Rectangle{
+    fn try_from(inputs : [&str; 3]) -> Result<Self> {
+        // Try from because what if we cannot parse the 
+        // slice into a u16?
+        Ok(Self {
+            length : inputs[0].parse()?,
+            height : inputs[1].parse()?,
+            width : inputs[2].parse()?
+        })
+    }
+}
+
+
+impl Rectangle{
+    /// Determing the total suface area of rectangular box
+    fn calculate_surface_area(&self) -> u64 {
+        2*self.length*self.width + 2*self.width*self.height + 2*self.length*self.height
+    }
+    /// Determine the smallest two measurements
+    fn determine_smallest_combination_of_measurements(&self) -> (u64, u64) {
+        let mut measurements = [self.length, self.width, self.height];
+        measurements.sort();
+        (measurements[0], measurements[1])
+    }
+    /// Determine the smallest side and its individual area
+    /// using the two smallest measurements
+    fn calculate_smallest_surface(&self) -> u64 {
+        let (m1, m2) = self.determine_smallest_combination_of_measurements();
+        m1*m2
+    }
+    /// Determine the required wrapper paper
+    fn required_wrapping_paper(&self) -> u64 {
+        let mut surface_area = self.calculate_surface_area();
+        surface_area += self.calculate_smallest_surface();
+        surface_area
+    }
+}
+
+
+impl Rectangle {
+    fn volume(&self) -> u64 {
+        self.length * self.width * self.height
     }
 
-    fn part2(_input: String, path: &String) -> String {
-        let mut sum = 0;
-        for line in read_lines(path) {
-            let v: Vec<i32> = line.split('x').map(|x| x.parse().unwrap()).collect();
-            let (x, y, z) = (v[0], v[1], v[2]);
-            let faces = vec![(2 * x) + (2 * y), (2 * y) + (2 * z), (2 * x) + (2 * z)];
-            sum += faces.iter().min().unwrap();
-            sum += x * y * z;
+    fn calculate_ribbon_for_perimeter(&self) -> u64 {
+        let (m1, m2) = self.determine_smallest_combination_of_measurements();
+        2*m1 + 2*m2
+    }
+
+    fn required_ribbon(&self) -> u64 {
+        let mut ribbon_length = self.calculate_ribbon_for_perimeter();
+        ribbon_length += self.volume();
+        ribbon_length
+    }
+}
+
+
+impl Solve for D02 {
+    fn part1(_: String, path: &String) -> String {
+        let input = file_reader::read_lines(path);
+        let mut required_paper : u64 = 0;
+
+        for i in &input {
+            let measurements : Vec<&str> = i.split("x").collect();
+            let rectangle = Rectangle::try_from(
+                [measurements[0], measurements[1],measurements[2]]
+            ).expect("Could not parse measurements into Rectangle struct");
+
+            required_paper += rectangle.required_wrapping_paper();
         }
-        return sum.to_string();
+
+        required_paper.to_string()
+    }
+
+    fn part2(_: String, path: &String) -> String {
+        let input = file_reader::read_lines(path);
+        let mut required_ribbon : u64 = 0;
+
+        required_ribbon += input.into_iter()
+            .map(|i| {
+                let measurements : Vec<&str> = i.split("x").collect();
+                let rectangle = Rectangle::try_from(
+                [measurements[0], measurements[1],measurements[2]]
+                ).expect("Could not parse measurements into Rectangle struct");
+
+                rectangle.required_ribbon()
+            })
+            .sum::<u64>();
+
+        required_ribbon.to_string()
     }
 }
